0d97d992f9d2cc9bbd98c8ee137e2c68e8d3e147
Marley Boone <mboone30@ivyhtech.edu>
2021-03-03 14:58:26 -0500

    Added Movie class

--- MovieRater.Data/Review.cs
+++ MovieRater.Data/Review.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieRater.Data
 {
-    public class Class1
+    public class Review
     {
+        [Key]
+        public int ReviewId { get; set; }
     }
 }
