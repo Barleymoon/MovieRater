138f97f06d3509a3324ad7e94d9267c361020209
Benjamin Chamness <bjchamness@yahoo.com>
2021-03-03 15:05:03 -0500

    Show class

---
+++ MovieRater.Data/Show.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Data
+{
+    public class Show
+    {
+        [Key]
+        [Required]
+        public int ShowId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Rating { get; set; }
+        public string Genre { get; set; }
+
+        public List<string> Actors { get; set; }
+    }
+}
