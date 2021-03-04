a1d123dc6d3095767e892f38329d35f07d7c7575
e-lajeunesse <eric.lajeunesse@outlook.com>
2021-03-03 19:42:44 -0500

    Deleted migrations

--- MovieRater.Model/MovieCreate.cs
+++ MovieRater.Model/MovieCreate.cs
@@ -14,5 +14,6 @@ namespace MovieRater.Model
         public string Title { get; set; }
         public string Description { get; set; }
         public List<string> Actors { get; set; }
+        public string Genre { get; set; }
     }
 }
